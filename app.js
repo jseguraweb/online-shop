ea0ab7758387cfc7b0f6dea4be1a192dccfe18a2
Jaime Segura <jseguraweb@gmail.com>
2020-04-24 17:29:48 +0200

    create react app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const indexRoute = require('./routes/indexRoute');
 const eyeGlassesRoute = require('./routes/eyeGlassesRoute');
 const sunGlassesRoute = require('./routes/sunGlassesRoute');
