ae1dff9491a506440ddcfc78fd2648b2138b36fa
Jaime Segura <jseguraweb@gmail.com>
2020-04-26 00:19:29 +0200

    add more inputs in the registrations form

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -13,7 +13,7 @@ const Register = () => {
                 </label>
             </form>
             <h4>Delivery address:</h4>
-            <form>
+            <form className="data-container">
                 <label htmlFor="first-name" className="first-name">
                     <input type="text" id="first-name" name="first-name" />
                 </label>
@@ -23,16 +23,21 @@ const Register = () => {
                 <label htmlFor="address" className="address">
                     <input type="text" id="address" name="address" />
                 </label>
-                <label htmlFor="" className="address-nr">
+                <label htmlFor="address-nr" className="address-nr">
                     <input type="number" id="address-nr" name="address-nr" />
                 </label>
-                <label htmlFor="" className="address-post">
+                <label htmlFor="address-post" className="address-post">
                     <input type="number" id="address-post" name="address-post" />
+                </label>
+                <label htmlFor="city" className="city">
+                    <input type="text" id="city" name="city" />
+                </label>
+                <label htmlFor="country" className="country">
+                    <input type="text" id="country" name="country" />
                 </label>
                 <label htmlFor="" className="submit-btn">
                     <input type="submit" value="PAY" id="submit-btn" className="active-button" />
                 </label>
-
             </form>
         </Fragment>
     );
