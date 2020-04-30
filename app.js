220709e5656e8f16c31f1e5af09d42525f572f4b
Jaime Segura <jseguraweb@gmail.com>
2020-04-30 15:54:32 +0200

    modify package

--- app.js
+++ app.js
@@ -25,12 +25,11 @@ const cartRoute = require('./routes/cartRoute');
 const profileRoute = require('./routes/profileRoute');
 const paymentRoute = require('./routes/paymentRoute');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 // app.use(express.static(path.resolve(__dirname, 'client')));
 app.use(express.json());
